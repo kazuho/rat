362a643c1a91e5ff7a6a84496385db08a0cc108b
Kazuho Oku <kazuhooku@gmail.com>
2022-12-25 15:41:58 +0900

    start writing NAT

---
+++ nat.rb
@@ -0,0 +1,124 @@
+require "./tun"
+
+class NATTable
+    attr_accessor :idle_timeout, :global_ports
+
+    class Entry
+        attr_accessor :prev, :next, :last_access, :local_addr, :local_port, :global_port, :remote_addr, :remote_port
+
+        def link(anchor)
+            last_access = Time.now.to_i
+            @anchor.prev.next = entry
+            entry.prev = @anchor.prev
+            @anchor.prev = entry
+            entry.next = @anchor
+        end
+
+        def unlink()
+            prev.next = self.next
+            self.next.prev = prev
+            prev = nil
+            self.next = nil
+        end
+    end
+
+    def initialize()
+        @anchor = Entry.new
+        @anchor.prev = @anchor
+        @anchor.next = @anchor
+        @locals = {}  # index of entries with key: local_addr + local_port + remote_addr + remote_port
+        @remotes = {} # index of entries with key: global_port + remote_addr + remote_port
+        @global_ports = []
+    end
+
+    def lookup_egress(local_addr, local_port, remote_addr, remote_port)
+        entry = @locals[local_key(local_addr, local_port, remote_addr, remote_port)]
+
+        if entry.nil?
+            global_port = empty_port(remote_addr, remote_port)
+            entry = _insert(local_addr, local_port, global_port, remote_addr, remote_port)
+        else
+            entry.unlink
+            entry.link(@anchor)
+        end
+
+        entry.global_port
+    end
+
+    def lookup_ingress(global_port, remote_addr, remote_port)
+        @remotes[remote_key(global_port, remote_addr, remote_port)]
+    end
+
+    def empty_port(remote_addr, remote_port)
+        gc
+        loop do
+            test_port = @global_ports[rand(@global_ports.length)]
+            unless @remotes[remote_key(test_port, remote_addr, remote_port)]
+                return test_port
+            end
+        end
+    end
+
+    def gc()
+        items_before = Time.now.to_i - idle_timeout
+        while @anchor.next != @anchor && @anchor.last_access < items_before
+            entry.unlink
+            @locals.delete(local_key(entry.local_addr, entry.local_port, entry.remote_addr, entry.remote_port))
+            @remotes.delete(remote_key(entry.global_port, entry.remote_addr, entry.remote_port))
+        end
+    end
+
+    def _insert(local_addr, local_port, global_port, remote_addr, remote_port)
+        entry = Entry.new
+        entry.local_addr = local_addr
+        entry.local_port = local_port
+        entry.global_port = global_port
+        entry.remote_addr = remote_addr
+        entry.remote_port = remote_port
+
+        entry.link(@anchor)
+        @locals[local_key(local_addr, local_port, remote_addr, remote_key)] = entry
+        @remotes[remote_key(global_port, remote_addr, + remote_port)] = entry
+
+        entry
+    end
+
+    def local_key(local_addr, local_port, remote_addr, remote_port)
+        local_addr + ":" + local_port + ":" + remote_addr + ":" + remote_port
+    end
+
+    def remote_key(global_port, remote_addr, remote_port)
+       global_port + "+" + remote_addr + ":" + remote_port
+    end
+end
+
+GLOBAL_ADDR = "\xa\1\2\4"
+tcp_table = NATTable.new
+tcp_table.idle_timeout = 30
+tcp_table.global_ports.push *(1024..65535)
+
+tun = Tun.new("tap-e")
+
+loop do
+    packet = tun.read()
+    if packet.l4
+        if packet.l4.is_a?(TCP)
+            if is_egress(packet)
+                global_port = tcp_table.lookup_egress(packet.l3.src_addr, packet.l4.src_port, packet.l3.dest_addr,
+                                                      packet.l4.dest_port)
+                packet.l3.src_addr = GLOBAL_ADDR
+                packet.l4.src_port = global_port
+                packet.apply
+                tun.write(packet)
+            else
+                tuple = tcp._table.lookup_ingress(packet.l4.dest_port, packet.l3.src_addr, packet.l3.src_port)
+                if tuple
+                    packet.l3.dest_addr = tuple.local_addr
+                    packet.l4.dest_port = tuple.local_port
+                    packet.apply
+                    tun.write(packet)
+                end
+            end
+        end
+    end
+end
