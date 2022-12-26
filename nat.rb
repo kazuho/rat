6ebd8ab886bdaa51fc87193ad59487701f2079ee
Kazuho Oku <kazuhooku@gmail.com>
2022-12-26 14:05:31 +0900

    invoke each iteration as a function so that yjit would compile it

--- nat.rb
+++ nat.rb
@@ -133,26 +133,26 @@ class ConeNAT < BaseNAT
 end
 
 GLOBAL_ADDR = "\xc0\xa8\x5b\xc8".b
-tcp_table = SymmetricNAT.new("tcp")
-tcp_table.idle_timeout = 120
-tcp_table.global_ports.push *(9000 .. 9099)
-udp_table = ConeNAT.new("udp")
-udp_table.idle_timeout = 30
-udp_table.global_ports.push *(9000 .. 9099)
+$tcp_table = SymmetricNAT.new("tcp")
+$tcp_table.idle_timeout = 300
+$tcp_table.global_ports.push *(9000 .. 9099)
+$udp_table = ConeNAT.new("udp")
+$udp_table.idle_timeout = 30
+$udp_table.global_ports.push *(9000 .. 9099)
 
 def is_egress(packet)
     packet.l3.dest_addr != GLOBAL_ADDR
 end
 
-tun = Tun.new("rat")
+$tun = Tun.new("rat")
 
-loop do
-    packet = tun.read()
+def doit()
+    packet = $tun.read()
     if packet.l4
         if packet.l4.is_a?(TCP)
-            table = tcp_table
+            table = $tcp_table
         elsif packet.l4.is_a?(UDP)
-            table = udp_table
+            table = $udp_table
         end
         if table
             if is_egress(packet)
@@ -163,7 +163,7 @@ loop do
                     packet.l3.src_addr = GLOBAL_ADDR
                     packet.l4.src_port = global_port
                     packet.apply
-                    tun.write(packet)
+                    $tun.write(packet)
                 end
             else
                 tuple = table.lookup_ingress(packet.l4.dest_port, packet.l3.src_addr, packet.l4.src_port)
@@ -171,11 +171,15 @@ loop do
                     packet.l3.dest_addr = tuple.local_addr
                     packet.l4.dest_port = tuple.local_port
                     packet.apply
-                    tun.write(packet)
+                    $tun.write(packet)
                 else
                     puts "#{table.name}:drop ingress to port #{packet.l4.dest_port}"
                 end
             end
         end
     end
+end
+
+loop do
+    doit()
 end
