b32ee0eb8dd266347f17ede88a86c53ab404b8e1
Kazuho Oku <kazuhooku@gmail.com>
2022-12-27 15:04:17 +0900

    config vs. code

--- rat.rb
+++ rat.rb
@@ -2,28 +2,14 @@ require "./tun"
 require "./nattable"
 
 class Rat
-    GLOBAL_ADDR = "\xc0\xa8\x0\x89".b
+    attr_accessor :global_addr, :tcp_table, :udp_table, :icmp_echo_table
 
-    attr_accessor :tcp_table, :udp_table, :icmp_echo_table
-
-    def initialize()
-        @tcp_table = SymmetricNATTable.new("tcp")
-        @tcp_table.idle_timeout = 300
-        @tcp_table.global_ports.push *(9000 .. 9099)
-
-        @udp_table = ConeNATTable.new("udp")
-        @udp_table.idle_timeout = 30
-        @udp_table.global_ports.push *(9000 .. 9999)
-
-        @icmp_echo_table = SymmetricNATTable.new("icmp-echo")
-        @icmp_echo_table.idle_timeout = 30
-        @icmp_echo_table.global_ports.push *(9000 .. 9999)
-
-        @tun = Tun.new("rat")
+    def initialize(devname)
+        @tun = Tun.new(devname)
     end
 
     def is_egress(packet)
-        packet.dest_addr != GLOBAL_ADDR
+        packet.dest_addr != @global_addr
     end
 
     def run()
@@ -44,13 +30,14 @@ class Rat
             handle_destunreach(packet)
             return
         end
+        return if table.nil?
 
         if is_egress(packet)
             global_port = table.lookup_egress(packet)
             if global_port.nil?
                 puts "#{table.name}:no empty port"
             else
-                packet.src_addr = GLOBAL_ADDR
+                packet.src_addr = @global_addr
                 packet.l4.src_port = global_port
                 packet.apply
                 @tun.write(packet)
@@ -73,9 +60,9 @@ class Rat
             table = @tcp_table
         elsif packet.l4.original.l4.is_a?(UDP)
             table = @udp_table
-        else
-            return
         end
+        return if table.nil?
+
         entry = table.lookup_ingress3(packet.l4.original.l4.src_port, packet.l4.original.dest_addr, packet.l4.original.l4.dest_port)
         if entry.nil?
             puts "drop ICMP destination unreachable to port #{packet.l4.original.l4.src_port}"
@@ -91,7 +78,22 @@ class Rat
     end
 end
 
-rat = Rat.new
+rat = Rat.new("rat")
+
+rat.global_addr = "\xc0\xa8\x0\x89".b
+
+rat.tcp_table = SymmetricNATTable.new("tcp")
+rat.tcp_table.idle_timeout = 300
+rat.tcp_table.global_ports.push *(9000 .. 9099)
+
+rat.udp_table = ConeNATTable.new("udp")
+rat.udp_table.idle_timeout = 30
+rat.udp_table.global_ports.push *(9000 .. 9999)
+
+rat.icmp_echo_table = SymmetricNATTable.new("icmp-echo")
+rat.icmp_echo_table.idle_timeout = 30
+rat.icmp_echo_table.global_ports.push *(9000 .. 9999)
+
 loop do
     rat.run()
 end
